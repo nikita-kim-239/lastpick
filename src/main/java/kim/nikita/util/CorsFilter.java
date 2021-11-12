fb8ddec6f80df7a982dac0137431234de295d1c8
Nikita <halflifer@mail.ru>
2021-11-12 17:25:05 +0300

    fix heroku cors filter

---
+++ src/main/java/kim/nikita/util/CorsFilter.java
@@ -0,0 +1,56 @@
+package kim.nikita.util;
+
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+public class CorsFilter implements Filter {
+
+
+    public CorsFilter() {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+
+
+        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "*");
+        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST");
+
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+
+
+        if (request.getMethod().equals("OPTIONS")) {
+            resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+            return;
+        }
+
+
+        chain.doFilter(request, servletResponse);
+    }
+
+    @Override
+    public void init(FilterConfig fConfig) throws ServletException {
+
+    }
+
+}
